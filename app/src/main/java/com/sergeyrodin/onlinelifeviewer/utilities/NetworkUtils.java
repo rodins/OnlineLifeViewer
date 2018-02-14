d759190850b6c0a5d5df2c548de66ccf045ca4c5
Sergey Rodin <rodin.s@rambler.ru>
2018-02-14 16:23:52 +0200

    Add getResponseFromHttpUrl to networkUtils. Update gradle

--- app/src/main/java/com/sergeyrodin/onlinelifeviewer/utilities/NetworkUtils.java
+++ app/src/main/java/com/sergeyrodin/onlinelifeviewer/utilities/NetworkUtils.java
@@ -2,10 +2,14 @@ package com.sergeyrodin.onlinelifeviewer.utilities;
 
 import android.net.Uri;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Scanner;
 
 /**
  * Created by sergey on 30.11.17.
@@ -81,5 +85,24 @@ public class NetworkUtils {
                 .appendQueryParameter(PARAM_NEWS_ID, Integer.toString(id))
                 .build();
         return builtUri.toString();
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if(hasInput) {
+                return scanner.next();
+            }else {
+                return null;
+            }
+        }finally {
+            urlConnection.disconnect();
+        }
     }
 }
