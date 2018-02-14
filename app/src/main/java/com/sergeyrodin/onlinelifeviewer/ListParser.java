d67fd7140d0842e92101a281f543ddc10a2be606
Sergey Rodin <rodin.s@rambler.ru>
2018-02-14 18:51:53 +0200

    Use getResponseFromHttpUrl for getting playlist json

--- app/src/main/java/com/sergeyrodin/onlinelifeviewer/ListParser.java
+++ app/src/main/java/com/sergeyrodin/onlinelifeviewer/ListParser.java
@@ -1,5 +1,7 @@
 package com.sergeyrodin.onlinelifeviewer;
 
+import com.sergeyrodin.onlinelifeviewer.utilities.NetworkUtils;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.regex.Matcher;
@@ -13,7 +15,7 @@ class ListParser {
         Matcher m = Pattern.compile("\"pl\":\"(.+?)\"").matcher(page);
         if(m.find()){
             URL url = new URL(m.group(1));
-            return new Curl().getJsLinkString(url);
+            return NetworkUtils.getResponseFromHttpUrl(url);
         }
         return null;
     }
