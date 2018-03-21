e7028c3148133fa28866d5d01603552a38b1b866
Sergey Rodin <rodin.s@rambler.ru>
2018-03-21 21:43:33 +0200

    Only show in dialog links with size found

--- app/src/main/java/com/sergeyrodin/onlinelifeviewer/PlayDialogFragment.java
+++ app/src/main/java/com/sergeyrodin/onlinelifeviewer/PlayDialogFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by root on 08.05.16.
@@ -20,24 +21,58 @@ public class PlayDialogFragment extends DialogFragment {
         String fileSize = mPsItem.getFileSize()!=null?" (" + mPsItem.getFileSize() + " Mb)":"";
         String downloadSize = mPsItem.getDownloadSize()!=null?" (" + mPsItem.getDownloadSize() + " Mb)":"";
 
-        String[] items = new String[] {
-          getString(R.string.flv) + fileSize,
-          getString(R.string.mp4) + downloadSize,
-        };
+        if(!fileSize.isEmpty() && !downloadSize.isEmpty()) {
+            String[] items = new String[] {
+                    getString(R.string.flv) + fileSize,
+                    getString(R.string.mp4) + downloadSize,
+            };
 
-        builder.setItems(items, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                switch(which) {
-                    case 0://Play
-                        ProcessPlaylistItem.show(getActivity(), mPsItem.getFile());
-                        break;
-                    case 1://Download
-                        ProcessPlaylistItem.show(getActivity(), mPsItem.getDownload());
-                        break;
+            builder.setItems(items, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch(which) {
+                        case 0://Flv
+                            ProcessPlaylistItem.show(getActivity(), mPsItem.getFile());
+                            break;
+                        case 1://Mp4
+                            ProcessPlaylistItem.show(getActivity(), mPsItem.getDownload());
+                            break;
+                    }
                 }
-            }
-        });
+            });
+        }else if(!fileSize.isEmpty() && downloadSize.isEmpty()) {
+            String[] items = new String[] {
+                    getString(R.string.flv) + fileSize,
+            };
+
+            builder.setItems(items, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch(which) {
+                        case 0:// Flv
+                            ProcessPlaylistItem.show(getActivity(), mPsItem.getFile());
+                            break;
+                    }
+                }
+            });
+        }else if(!downloadSize.isEmpty() && fileSize.isEmpty()) {
+            String[] items = new String[] {
+                    getString(R.string.mp4) + downloadSize,
+            };
+
+            builder.setItems(items, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch(which) {
+                        case 0://Download
+                            ProcessPlaylistItem.show(getActivity(), mPsItem.getDownload());
+                            break;
+                    }
+                }
+            });
+        }else if(fileSize.isEmpty() && downloadSize.isEmpty()) {
+            builder.setTitle(R.string.no_links_found);
+        }
 
         return builder.create();
     }
