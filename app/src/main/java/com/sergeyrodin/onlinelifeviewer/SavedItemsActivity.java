bff1d909ec654daf7b1992b58d8b105574c9e5a4
Sergey Rodin <rodin.s@rambler.ru>
2018-09-03 13:17:06 +0300

    Change saved items layout and get refs to views in Activity

--- app/src/main/java/com/sergeyrodin/onlinelifeviewer/SavedItemsActivity.java
+++ app/src/main/java/com/sergeyrodin/onlinelifeviewer/SavedItemsActivity.java
@@ -2,12 +2,23 @@ package com.sergeyrodin.onlinelifeviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 public class SavedItemsActivity extends AppCompatActivity {
+    private RecyclerView mRvSaveItems;
+    private TextView mTvNoItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_items);
+
+        mRvSaveItems = findViewById(R.id.rv_saved_items);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRvSaveItems.setLayoutManager(layoutManager);
+
+        mTvNoItems = findViewById(R.id.tv_no_saved_items);
     }
 }
