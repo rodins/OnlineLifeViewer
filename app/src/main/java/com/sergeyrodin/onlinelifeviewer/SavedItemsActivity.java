1a9e2a6d2c50b35acfe83808b6cba245975d90dd
Sergey Rodin <rodin.s@rambler.ru>
2018-09-04 20:24:13 +0300

    Implement deletion of saved item by swipe

--- app/src/main/java/com/sergeyrodin/onlinelifeviewer/SavedItemsActivity.java
+++ app/src/main/java/com/sergeyrodin/onlinelifeviewer/SavedItemsActivity.java
@@ -7,10 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.sergeyrodin.onlinelifeviewer.database.AppDatabase;
 import com.sergeyrodin.onlinelifeviewer.database.SavedItem;
 
 import java.util.List;
@@ -50,6 +52,28 @@ public class SavedItemsActivity extends AppCompatActivity
                 }
             }
         });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            // Called when a user swipes left or right on a ViewHolder
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                final AppDatabase db = AppDatabase.getsInstanse(getApplicationContext());
+                // Here is where you'll implement swipe to delete
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        List<SavedItem> savedItems = mSavedItemsAdapter.getSavedItems();
+                        db.savedItemsDao().deleteSavedItem(savedItems.get(position));
+                    }
+                });
+            }
+        }).attachToRecyclerView(mRvSaveItems);
     }
 
     @Override
